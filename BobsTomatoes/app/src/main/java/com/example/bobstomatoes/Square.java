a4dfa9bf9936a7fccb9a2a7ed94082172d015aa8
Jake Hennig <jkhennig@ualberta.ca>
2022-10-01 23:17:31 -0600

    Recreate Square

--- BobsTomatoes/app/src/main/java/com/example/bobstomatoes/Square.java
+++ BobsTomatoes/app/src/main/java/com/example/bobstomatoes/Square.java
@@ -2,8 +2,9 @@ package com.example.bobstomatoes;
 
 // Square.java
 
-public class Square extends Shape {
-    public Square (int x, int y) {
-        super (x, y);
-    }
+    public class Square extends Shape {
+        public Square (int x, int y) {
+            super (x, y);
+        }
+
 }
